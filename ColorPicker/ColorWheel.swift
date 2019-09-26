e37df92278ee3d30de4af2943878eaaf146b0ed4
John Kouris <jdkouris@gmail.com>
2019-09-25 18:40:08 -0700

    Created files and set the class of the ColorWheel

---
+++ ColorPicker/ColorWheel.swift
@@ -0,0 +1,21 @@
+//
+//  ColorWheel.swift
+//  ColorPicker
+//
+//  Created by John Kouris on 9/25/19.
+//  Copyright © 2019 John Kouris. All rights reserved.
+//
+
+import UIKit
+
+class ColorWheel: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
